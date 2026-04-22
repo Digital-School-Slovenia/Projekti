851a8e53249ae1a9e98fe923ce2cdbf4d298834f
metij-bisol <matej.mencin@bisol-energija.si>
2026-04-22 16:21:17 +0200

    add: manca

--- Python/Python začetni/01_print_in_prvi_program/resitve/01_ucni_list_resitve.py
+++ Python/Python začetni/01_print_in_prvi_program/resitve/01_ucni_list_resitve.py
@@ -2,11 +2,11 @@
 
 # Namen: glavna delovna rešitev za učni list tega sklopa.
 
-print("Matej Mencin")
+print("Manca _ je _ super _ programerka.")
 print("Sirovi štruklji")
 print("Pozdrav svet, pozdrav vesolje, pozdrav WC")
 print("HAHAHA " * 10)
 
-ime = "Ana"
+ime = "Manca"
 najljubsi_predmet = "matematika"
 print(f"Živjo, jaz sem {ime} in moj najljubši predmet je {najljubsi_predmet}.")
