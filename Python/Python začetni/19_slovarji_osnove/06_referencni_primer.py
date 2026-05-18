2c124de07afb305e0a8bfbeac70db7a2733dd435
Mark Hočevar <mark.hocevar@gmail.com>
2026-05-18 21:13:59 +0200

    nepotrebni dokumenti

--- Python/Python začetni/19_slovarji_osnove/06_referencni_primer.py
+++
@@ -1,9 +0,0 @@
-# Referenčni primer – 19 Slovarji osnove
-
-# Namen: kratek referenčni primer za razlago glavne ideje tega sklopa.
-
-profil = {"ime": "Eva", "sledilci": 320, "objave": 15}
-
-
-for kljuc in profil.keys():
-    print(kljuc, "→", profil[kljuc])
